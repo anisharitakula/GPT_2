05e3fa7ccd2c8ab23fb04fa30c294770a9ba2eae
Anish Aritakula <anisharitakula@gmail.com>
2024-04-29 08:03:50 +1000

    Multi-head attention

--- train.py
+++ train.py
@@ -1,25 +1,31 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+
+
+
+
+split=0.9
+block_size=8
+batch_size=32
+#epochs=10
+learning_rate=1e-3
+max_iters=5000
+eval_interval=500
+eval_iters=200
+n_embed=32
+head_size=16
+n_attention_heads=4
+
 torch.manual_seed(1337)
 
 # read it in to inspect it
 with open('data.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
-text_length=len(text)
 vocab=sorted(list(set(text)))
 vocab_size=len(vocab)
-split=0.9
-block_size=8
-batch_size=32
-epochs=10
-learning_rate=1e-3
-max_iters=5200
-eval_iters=200
-n_embed=32
-head_size=16
-n_attention_heads=4
+text_length=len(text)
 
 stoi={s:i for i,s in enumerate(vocab)}
 itos={i:s for i,s in enumerate(vocab)}
@@ -30,9 +36,30 @@ decode=lambda x:''.join([itos[a] for a in x])
 data=torch.tensor(encode(text),dtype=torch.long)
 
 
-train_data=data[:round(split*text_length)]
-val_data=data[round(split*text_length):]
+train_data=data[:int(split*text_length)]
+val_data=data[int(split*text_length):]
 
+def get_batch(split):
+    # generate a small batch of data of inputs x and targets y
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+@torch.no_grad()
+def estimate_loss():
+    loss_dict={}
+    model.eval()
+    for split in ['train','val']:
+        losses=torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            x,y=get_batch(split)
+            logits,loss=model(x,y)
+            losses[k]=loss.item()
+        loss_dict[split]=losses.mean()
+    model.train()
+    return loss_dict
 
 
 class SelfAttention(nn.Module):
@@ -67,14 +94,14 @@ class MutiHeadAttention(nn.Module):
         x=torch.cat([h(x) for h in self.multihead],dim=-1)
         return x
 
-class BigramLanguageModel(nn.Module):
+class GPTLanguageModel(nn.Module):
 
     def __init__(self):
         super().__init__()
         self.token_embedding=nn.Embedding(vocab_size,
                                           n_embed)
         self.position_embedding=nn.Embedding(block_size,n_embed)
-        self.attention=SelfAttention(head_size)
+        self.multi_attention=MutiHeadAttention(n_attention_heads,head_size//n_attention_heads)
         self.ln1=nn.Linear(head_size,vocab_size)
     
     def forward(self,idx,targets=None):
@@ -86,15 +113,16 @@ class BigramLanguageModel(nn.Module):
         
         
         x=tok_emb + pos_emb #B,T,n_embed
-        x=self.attention(x)
+        x=self.multi_attention(x)
         logits=self.ln1(x)
         
         B,T,C=logits.shape
-        logits=logits.view(B*T,C)
+        
         
         if targets is None:
             loss=None
         else:
+            logits=logits.view(B*T,C)
             targets=targets.view(B*T)
             loss=F.cross_entropy(logits,targets)
         return logits,loss
@@ -107,9 +135,9 @@ class BigramLanguageModel(nn.Module):
             logits,loss=self(idx_cond)#(B,T,C)
             
             
-            logits=logits[-1,:].view(1,-1)#(B,C)
+            logits=logits[:,-1,:]#(B,C)
 
-            probs=F.softmax(logits,dim=1) #(B,C)
+            probs=F.softmax(logits,dim=-1) #(B,C)
             #idx_next=torch.argmax(probs,dim=1).view(-1,1) #(B,1)
             #If I use argmax, I am always getting the same character
             idx_next=torch.multinomial(probs,num_samples=1)
@@ -119,29 +147,8 @@ class BigramLanguageModel(nn.Module):
 
     
 
-def get_batch(split):
-    # generate a small batch of data of inputs x and targets y
-    data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
-    return x, y
 
-@torch.no_grad()
-def estimate_loss():
-    loss_dict={}
-    model.eval()
-    for split in ['train','val']:
-        losses=torch.zeros(eval_iters)
-        for k in range(eval_iters):
-            x,y=get_batch(split)
-            logits,loss=model(x,y)
-            losses[k]=loss.item()
-        loss_dict[split]=losses.mean()
-    model.train()
-    return loss_dict
-
-model=BigramLanguageModel()
+model=GPTLanguageModel()
 # x_batches,y_batches=get_batch('train')
 # logits,loss=model(x_batches,y_batches)
 # print(loss)
@@ -149,7 +156,7 @@ model=BigramLanguageModel()
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for iter in range(max_iters):
-    if iter%300==0:
+    if iter%eval_interval==0:
         losses=estimate_loss()
         print(f"Step {iter}: The train loss is {losses['train']} and val loss is {losses['val']}")
 
